c86787e248f8f6c4487603447f858080713860b8
jk <deliver0713@gmail.com>
2022-08-19 10:51:34 +0900

    projectapp

--- pinterest/pinterest/urls.py
+++ pinterest/pinterest/urls.py
@@ -17,14 +17,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from accountapp import views
 
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.hello_world, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
