9b7a60c257313263b1d0a7e8d4139acb9a7e6b80
jk <deliver0713@gmail.com>
2022-08-19 13:33:09 +0900

    subscription

---
+++ pinterest/subscribeapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import SubscriptionView
+
+app_name = "subscribeapp"
+
+
+
+
+urlpatterns = [
+    path('subscribe/', SubscriptionView.as_view(), name='subscribe')
+
+]
